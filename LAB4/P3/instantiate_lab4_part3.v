ee1a38b49357dd6dbd9969957923898cb9346fa5
adrianle2004 <160694561+adrianle2004@users.noreply.github.com>
2025-04-14 11:45:29 +1000

    lab4_3

--- LAB4/P3/instantiate_lab4_part3.v
+++ LAB4/P3/instantiate_lab4_part3.v
@@ -1,8 +1,13 @@
-module instantiate_lab4_part2(SW,LEDR);
+module instantiate_lab4_part3(SW,LEDR);
  
 	input[9:0] SW ;
 	output [9:0] LEDR;
 
 	// instantiate and connect master_slave
-	
+	master_slave ms (
+		.D(SW[0]),
+		.clk(SW[1]),
+		.Q(LEDR[0]),
+		.Qnot(LEDR[1]));
+		
 endmodule
--- LAB4/P3/master_slave.v
+++ LAB4/P3/master_slave.v
@@ -5,13 +5,15 @@ module master_slave (clk, D, Q, Qnot);
    wire Qm, Qnm;
 	
 	gated_DLatch master (
-		.clk(clk),
+		.clk(~clk),
 		.D(D),
-		.Q(Qm)
+		.Q(Qm),
 		.Qnot(Qnm)
 	);
-	gated_Dlatch slave (
-		.clk(~clk),
+	
+	
+	gated_DLatch slave (
+		.clk(clk),
 		.D(Qm),
 		.Q(Q),
 		.Qnot(Qnot)
