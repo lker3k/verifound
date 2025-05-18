46d4f73d2e2ebe58a5e3feaf4cfe55ab170e4b58
MATTHEWMCAULIFFE05 <148923067+MATTHEWMCAULIFFE05@users.noreply.github.com>
2025-05-19 09:48:25 +1000

    Add files via upload

---
+++ pc.v
@@ -0,0 +1,17 @@
+module program_counter (
+    input  wire clk,
+    input  wire rst_n,
+    input  wire branch,
+    input  wire [7:0] branchaddress,
+    output reg  [7:0] pc
+);
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            pc <= 8'b0;
+        else if (branch)
+            pc <= branchaddr;
+        else
+            pc <= pc + 1;
+    end
+endmodule
